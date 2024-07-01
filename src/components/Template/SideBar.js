9f188472a3acb568cd496e16334e93c8979a4f2e
Neel Patel <neel_patel2004@outlook.com>
2024-07-01 12:23:53 -0400

    Update SideBar.js

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -12,21 +12,17 @@ const SideBar = () => (
         <img src={`${PUBLIC_URL}/images/me.jpg`} alt="" />
       </Link>
       <header>
-        <h2>Michael D&apos;Angelo</h2>
-        <p><a href="mailto:michael.l.dangelo@gmail.com">michael.l.dangelo@gmail.com</a></p>
+        <h2>Neel Patel</h2>
+        <p><a href="neel_patel2004@outlook.com">neel_patel2004@outlook.com</a></p>
       </header>
     </section>
 
     <section className="blurb">
       <h2>About</h2>
-      <p>Hi, I&apos;m Michael. I like building things.
-        I am a <a href="https://icme.stanford.edu/">Stanford ICME</a> graduate, YC Alumni, and
-        the co-founder and CTO of <a href="https://arthena.com">Arthena</a>. Before Arthena I was
-        at <a href="https://matroid.com">Matroid</a>
-        , <a href="https://planet.com">Planet</a>
-        , <a href="https://planetaryresources.com">Planetary Resources</a>
-        , <a href="https://facebook.com">Facebook</a>
-        , and <a href="https://seds.org">SEDS</a>.
+      <p>Hi, I&apos;m Neel. I like deploying things.
+        I am a <a href="https://www.sheridancollege.ca/">Sheridan College</a> student. Before I was employee 
+        at <a href="https://www.futurewayinstitute.com/">Future Way</a> and 
+        , <a href="https://parmarboothouse.com/">Parmar Boot House</a>.
       </p>
       <ul className="actions">
         <li>
@@ -35,9 +31,9 @@ const SideBar = () => (
       </ul>
     </section>
 
-    <section id="footer">
+    <section id="footer"
       <ContactIcons />
-      <p className="copyright">&copy; Michael D&apos;Angelo <Link to="/">mldangelo.com</Link>.</p>
+      <p className="copyright">&copy; Neel Patel <Link to="/">neelpatelportfolio.com</Link>.</p>
     </section>
   </section>
 );
