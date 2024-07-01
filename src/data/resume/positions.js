c9963b983cfba291182a307d1f4252bc9a55a412
Neel Patel <neel_patel2004@outlook.com>
2024-07-01 00:02:19 -0400

    Update positions.js

--- src/data/resume/positions.js
+++ src/data/resume/positions.js
@@ -3,7 +3,7 @@ const positions = [
     company: 'Future Way Institute Of Multimedia',
     position: 'Software Technician',
     link: 'https://www.futurewayinstitute.com/',
-    daterange: '2023 - 2024',
+    daterange: '2023 - present',
     points: [
       'Deployed quantitative strategies to predict the value of fine art in various pricing contexts.',
       'Built production, on-line, end-to-end optimized machine learning pipelines with Pandas, Numpy, Scikit, Tensorflow, Dagster, Postgres, etc. on GCP with Kubernetes.',
@@ -15,7 +15,7 @@ const positions = [
     company: 'Parmar Boot House',
     position: 'Sales associate',
     link: 'https://parmarboothouse.com/',
-    daterange: '2022 - 2023',
+    daterange: '2022 - present',
     points: [
       'Developed strong product  knowledge to assist customers with informed purchasing decisions 
        Managed inventory ensuring optimal stock level and record-keeping 
