b7f5b0231b7367ac7fbf5c00e97b631add567167
Neel Patel <neel_patel2004@outlook.com>
2024-07-01 00:30:03 -0400

    Update projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -8,7 +8,7 @@ const data = [
       'Built for a social impact hackathon. '
       + 'NearestDollar connected to your bank accounts, credit cards, '
       + 'or debit cards and rounded up your purchases to donate the balance to '
-      + 'the charity of your choice.',}
+      + 'the charity of your choice.'},
 ];
 
 export default data;
