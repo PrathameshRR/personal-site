61a15e95f6755fde77fa780d1473cc1cbd7ff802
Neel Patel <neel_patel2004@outlook.com>
2024-07-01 00:34:57 -0400

    Update degrees.js

--- src/data/resume/degrees.js
+++ src/data/resume/degrees.js
@@ -3,13 +3,13 @@ const degrees = [
     school: 'Sheridan College',
     degree: 'Advanced Diploma',
     link: 'https://www.sheridancollege.ca/',
-    year: 2027
+    year: 2027,
   },
   {
     school: 'S.M.K.R Vashi High School',
     degree: 'Highe Secondary',
     link: 'https://schools.org.in/navsari/24240304503/s-m-k-r-vashi-high-school.html',
-    year: 2027
+    year: 2022,
   },
 ];
 
