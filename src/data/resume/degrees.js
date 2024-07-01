129176ba9195380645e8ddaab6c78fffd7f9d481
Neel Patel <neel_patel2004@outlook.com>
2024-06-30 21:28:16 -0400

    Update degrees.js

--- src/data/resume/degrees.js
+++ src/data/resume/degrees.js
@@ -1,15 +1,17 @@
 const degrees = [
   {
-    school: 'Stanford University',
-    degree: 'M.S. Computational and Mathematical Engineering',
-    link: 'https://stanford.edu',
-    year: 2016,
+    school: 'Sheridan College',
+    degree: 'Advanced Diploma',
+    field : 'Computer Engineering Technology'
+    link: 'https://www.sheridancollege.ca/',
+    year: May 2024 - April 2027,
   },
   {
-    school: 'University at Buffalo',
-    degree: 'B.S. Electrical Engineering, Computer Engineering',
-    link: 'https://buffalo.edu',
-    year: 2012,
+    school: 'S.M.K.R Vashi High School',
+    degree: 'Highe Secondary',
+    field: 'Science'
+    link: 'https://schools.org.in/navsari/24240304503/s-m-k-r-vashi-high-school.html',
+    year: June 2020 - April 2022,
   },
 ];
 
