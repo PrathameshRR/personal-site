d41ca17693ff20991ff1f63dd078ae64ba905d14
Neel Patel <neel_patel2004@outlook.com>
2024-07-01 00:33:52 -0400

    Update projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,6 +1,7 @@
 // TODO Add a couple lines about each project
 const data = [
- { title: 'Nearest Dollar',
+ { 
+  title: 'Nearest Dollar',
     subtitle: '2015 BVP Hackathon',
     image: '/images/projects/nearestdollar.jpg',
     date: '2015-11-20',
@@ -8,7 +9,8 @@ const data = [
       'Built for a social impact hackathon. '
       + 'NearestDollar connected to your bank accounts, credit cards, '
       + 'or debit cards and rounded up your purchases to donate the balance to '
-      + 'the charity of your choice.'},
+      + 'the charity of your choice.'
+ },
 ];
 
 export default data;
