56d27ffd93deddec02825504123b5a3ed72a51b6
Neelpatel1604 <neel_patel2004@outlook.com>
2024-08-30 19:00:22 -0400

    update projects

--- src/components/Projects/Cell.js
+++ src/components/Projects/Cell.js
@@ -2,29 +2,37 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import dayjs from 'dayjs';
 
-const Cell = ({ data }) => (
-  <div className="cell-container">
-    <article className="mini-post">
-      <header>
-        <h3><a href={data.link}>{data.title}</a></h3>
-        <time className="published">{dayjs(data.date).format('MMMM, YYYY')}</time>
-      </header>
-      <a href={data.link} className="image">
-        <img src={`${process.env.PUBLIC_URL}${data.image}`} alt={data.title} />
-      </a>
-      <div className="description">
-        <p>{data.desc}</p>
-      </div>
-    </article>
-  </div>
-);
+const Cell = ({ data }) => {
+  const formattedStartDate = dayjs(data.startDate).format('MMMM, YYYY');
+  const formattedEndDate = dayjs(data.endDate).format('MMMM, YYYY');
+  
+  return (
+    <div className="cell-container">
+      <article className="mini-post">
+        <header>
+          <h3><a href={data.link}>{data.title}</a></h3>
+          <time className="published">
+            {formattedStartDate} - {formattedEndDate}
+          </time>
+        </header>
+        <a href={data.link} className="image">
+          <img src={`${process.env.PUBLIC_URL}${data.image}`} alt={data.title} />
+        </a>
+        <div className="description">
+          <p>{data.desc}</p>
+        </div>
+      </article>
+    </div>
+  );
+};
 
 Cell.propTypes = {
   data: PropTypes.shape({
     title: PropTypes.string.isRequired,
     link: PropTypes.string,
     image: PropTypes.string.isRequired,
-    date: PropTypes.string.isRequired,
+    startDate: PropTypes.string.isRequired,
+    endDate: PropTypes.string.isRequired,
     desc: PropTypes.string.isRequired,
   }).isRequired,
 };
--- src/data/projects.js
+++ src/data/projects.js
@@ -1,16 +1,23 @@
 // TODO Add a couple lines about each project
 const data = [
   {
-    title: 'Nearest Dollar',
-    subtitle: '2015 BVP Hackathon',
+    title: 'Deployment Of ReactJS',
+    subtitle: 'Deployement by using Docker and EC2',
     image: '/images/projects/nearestdollar.jpg',
-    date: '2015-11-20',
+    link:'https://blog.neelbuilds.com/deployment-for-react-js',
+    startDate: '2024-08-19',
+    endDate: '2024-08-19',
     desc:
-      'Built for a social impact hackathon. '
-      + 'NearestDollar connected to your bank accounts, credit cards, '
-      + 'or debit cards and rounded up your purchases to donate the balance to '
-      + 'the charity of your choice.',
+      'This project focuses on deploying React.js applications with a detailed, step-by-step approach.'
+      + 'It guides you through preparing your React app for production, selecting an appropriate hosting platform, and configuring deployment settings. '
+      + 'The project also includes tips on optimizing performance and ensuring a seamless deployment process.'
+      + 'Whether you are deploying your first React app or refining your workflow, this project provides the insights and tools needed for successful deployment.',
   },
 ];
 
 export default data;
+//This project focuses on deploying React.js applications with a detailed, step-by-step approach. It guides you 
+//through preparing your React app for production, selecting an appropriate hosting platform, 
+//and configuring deployment settings. The project also includes tips on optimizing performance and ensuring
+// a seamless deployment process. Whether you're deploying your 
+//first React app or refining your workflow, this project provides the insights and tools needed for successful deployment.
