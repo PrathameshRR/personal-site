32db632f25f1d60d106251806c9b9a4f8a83d3e7
Neelpatel1604 <neel_patel2004@outlook.com>
2024-08-31 19:58:14 -0400

    update event

--- src/data/event.js
+++ src/data/event.js
@@ -1,62 +1,52 @@
 const data = [
-{
-    title: 'K8SUG- Kubernetes User Group-10th',
+  {
+    title: "K8SUG- Kubernetes User Group-10th",
     images: [
-    'https://photosevent.s3.amazonaws.com/k1.jpeg',
-    'https://photosevent.s3.amazonaws.com/k1-2.jpeg',
-    'https://photosevent.s3.amazonaws.com/k1-3.jpeg',
+      "https://photosevent.s3.amazonaws.com/k1.jpeg",
+      "https://photosevent.s3.amazonaws.com/k1-2.jpeg",
+      "https://photosevent.s3.amazonaws.com/k1-3.jpeg",
     ],
-    date: '2024-06-30',
+    date: "2024-06-30",
     desc: "Attending my first Tech Meetup at Toronto's AWS Office was amazing! Key takeaways included hands-on experience with Red Hat OpenShift and valuable networking opportunities.",
-},
-{
-    title: 'ADPlist Meetup',
+  },
+  {
+    title: "GDEX-Global DevOps Experience Toronto",
     images: [
-    'https://photosevent.s3.amazonaws.com/2+june+adp.jpeg',
+      "https://photosevent.s3.amazonaws.com/M-1.jpg",
+      "https://photosevent.s3.amazonaws.com/M-2.jpg",
+      "https://photosevent.s3.amazonaws.com/M-3.jpg",
+      "https://photosevent.s3.amazonaws.com/M-4.jpg",
     ],
-    date: '2024-06-02',
-    desc: 'The event was a fantastic gathering of designers, developers, and professionals exchanging ideas and learning about cross-departmental and domain knowledge.',
-},
-{
-    title: 'GDEX-Global DevOps Experience Toronto',
+    date: "2024-06-15",
+    desc: "The Global DevOps Experience event at Microsoft HQ in Toronto provided valuable insights into GitHub and Azure, with impactful hands-on experiences and a strong focus on team collaboration.",
+  },
+  {
+    title: "K8SUG- Kubernetes User Group-11th",
     images: [
-    'https://photosevent.s3.amazonaws.com/M-1.jpg',
-    'https://photosevent.s3.amazonaws.com/M-2.jpg',
-    'https://photosevent.s3.amazonaws.com/M-3.jpg',
-    'https://photosevent.s3.amazonaws.com/M-4.jpg',
+      "https://photosevent.s3.amazonaws.com/k2-1.jpeg",
+      "https://photosevent.s3.amazonaws.com/k2-2.jpeg",
+      "https://photosevent.s3.amazonaws.com/k2-3.jpeg",
     ],
-    date: '2024-06-15',
-    desc: 'The Global DevOps Experience event at Microsoft HQ in Toronto provided valuable insights into GitHub and Azure, with impactful hands-on experiences and a strong focus on team collaboration.',
-},
-{
-    title: 'K8SUG- Kubernetes User Group-11th',
+    date: "2024-06-25",
+    desc: "The experience was enriching, featuring a workshop on Gen AI Workloads with Amazon EKS, a demo on Gen AI Modeling with Amazon EKS and Karpenter, and a session on Securing Kubernetes with Aqua Security.",
+  },
+  {
+    title: "InnocaS.E Next Gen Case Study Challenge",
     images: [
-    'https://photosevent.s3.amazonaws.com/k2-1.jpeg',
-    'https://photosevent.s3.amazonaws.com/k2-2.jpeg',
-    'https://photosevent.s3.amazonaws.com/k2-3.jpeg',
+      "https://photosevent.s3.amazonaws.com/Enterprenuership.jpg",
+      "https://photosevent.s3.amazonaws.com/Entreprenuership+certificate.jpg",
+      "https://photosevent.s3.amazonaws.com/Entreprenuership-2.png",
+      "https://photosevent.s3.amazonaws.com/Entreprenuership-3.png",
     ],
-    date: '2024-06-25',
-    desc: 'The experience was enriching, featuring a workshop on Gen AI Workloads with Amazon EKS, a demo on Gen AI Modeling with Amazon EKS and Karpenter, and a session on Securing Kubernetes with Aqua Security.',
-},
-{
-    title: 'InnocaS.E Next Gen Case Study Challenge',
-    images: [
-    'https://photosevent.s3.amazonaws.com/Enterprenuership.jpg',
-    'https://photosevent.s3.amazonaws.com/Entreprenuership+certificate.jpg',
-    'https://photosevent.s3.amazonaws.com/Entreprenuership-2.png',
-    'https://photosevent.s3.amazonaws.com/Entreprenuership-3.png',
-    ],
-    date: '2024-06-26',
-    desc: 'We were selected among the top 12 teams out of 25 and 100 participants. We gained invaluable insights into innovative problem-solving, received constructive feedback from industry experts, and had the chance to engage with diverse ideas and approaches.',
-},
-{
-    title: 'ADPlist Meetup',
-    images: [
-    'https://photosevent.s3.amazonaws.com/13+july+adp.jpeg',
-    ],
-    date: '2024-07-13',
+    date: "2024-06-26",
+    desc: "We were selected among the top 12 teams out of 25 and 100 participants. We gained invaluable insights into innovative problem-solving, received constructive feedback from industry experts, and had the chance to engage with diverse ideas and approaches.",
+  },
+  {
+    title: "ADPlist Meetup",
+    images: ["https://photosevent.s3.amazonaws.com/13+july+adp.jpeg"],
+    date: "2024-07-13",
     desc: "We explored integrating design with technology and learned about new tools and methodologies. The event highlighted the importance of continuous learning and staying updated with industry advancements.",
-},
+  },
 ];
-  
+
 export default data;
