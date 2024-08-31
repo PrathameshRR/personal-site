7ad3fc644791bf1477367a2322943a28bb5a39a6
Neelpatel1604 <neel_patel2004@outlook.com>
2024-08-31 15:43:02 -0400

    update events

--- src/data/event.js
+++ src/data/event.js
@@ -1,18 +1,17 @@
 const data = [
-    {
-      title: 'First Event',
-      subtitle: 'Deployement by using Docker and EC2',
-      image: '/images/projects/Deploymentofreactjsapp.png',
-      link: 'https://blog.neelbuilds.com/deployment-for-react-js',
-      startDate: '2024-08-19',
-      endDate: '2024-08-19',
-      desc:
+{
+  title: 'First Event',
+  subtitle: 'Deployement by using Docker and EC2',
+  image: '/images/projects/Deploymentofreactjsapp.png',
+  link: 'https://blog.neelbuilds.com/deployment-for-react-js',
+  startDate: '2024-08-19',
+  endDate: '2024-08-19',
+  desc:
         'This project guides the deployment of React.js apps.'
         + 'It covers preparation, hosting, configuration, and focuses  '
         + 'on performance optimization to '
         + 'ensure a seamless deployment process.',
-    },
-  ];
-  
-  export default data;
-  
+},
+];
+
+export default data;
