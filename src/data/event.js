8e13647fe8df3c498f763eaa7c1d683e1e0a9915
Neelpatel1604 <neel_patel2004@outlook.com>
2024-08-31 15:52:08 -0400

    update event

--- src/data/event.js
+++ src/data/event.js
@@ -1,32 +1,17 @@
 const data = [
-<<<<<<< HEAD
   {
-    title: 'First Event',
+    title: 'Deployment Of ReactJS',
     subtitle: 'Deployement by using Docker and EC2',
     image: '/images/projects/Deploymentofreactjsapp.png',
     link: 'https://blog.neelbuilds.com/deployment-for-react-js',
     startDate: '2024-08-19',
     endDate: '2024-08-19',
     desc:
-=======
-{
-  title: 'First Event',
-  subtitle: 'Deployement by using Docker and EC2',
-  image: '/images/projects/Deploymentofreactjsapp.png',
-  link: 'https://blog.neelbuilds.com/deployment-for-react-js',
-  startDate: '2024-08-19',
-  endDate: '2024-08-19',
-  desc:
->>>>>>> a15d62a3e014a011bc96a993c1f34bdbc6ac756e
         'This project guides the deployment of React.js apps.'
         + 'It covers preparation, hosting, configuration, and focuses  '
         + 'on performance optimization to '
         + 'ensure a seamless deployment process.',
-<<<<<<< HEAD
   },
-=======
-},
->>>>>>> a15d62a3e014a011bc96a993c1f34bdbc6ac756e
 ];
 
 export default data;
