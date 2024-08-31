25bf5460e032393eef50e7bb3990f41d73f7689c
Neelpatel1604 <neel_patel2004@outlook.com>
2024-08-31 01:17:01 -0400

    update certifications

--- src/data/resume/certifications.js
+++ src/data/resume/certifications.js
@@ -3,9 +3,7 @@ const certifications = [
     title: 'AWS Certified Cloud Practitioner',
     organization: 'Amazon Web Services',
     year: '2024',
-    link: 'https://www.credly.com/badges/a1a0c75d-b2bb-4f82-b97e-a67af59589b5/linked_in?t=shv1tc',  // Add the link to the certification
+    link: 'https://www.credly.com/badges/a1a0c75d-b2bb-4f82-b97e-a67af59589b5/linked_in?t=shv1tc',// Add the link to the certification
   },
-];
-  
-export default certifications;
-  
+];  
+export default certifications; 
