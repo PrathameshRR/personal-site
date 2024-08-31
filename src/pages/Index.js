51ba1fcc566927f9a1810ee353381aa49c2811be
Neelpatel1604 <neel_patel2004@outlook.com>
2024-08-31 02:21:20 -0400

    update index.js

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -4,8 +4,10 @@ import Main from '../layouts/Main';
 
 const Index = () => (
   <Main
-    description={"Neel patel's personal website. Studing at Sheridan College, "
-    + 'Devloping knowledge and experience in Cloud computing'}
+    description={
+      "Neel patel's personal website. Studying at Sheridan College, " +
+      'Developing knowledge and experience in Cloud computing'
+    }
   >
     <article className="post" id="index">
       <header>
@@ -13,13 +15,32 @@ const Index = () => (
           <h2 data-testid="heading"><Link to="/">Welcome to My Site</Link></h2>
         </div>
       </header>
-      <p> Welcome to my website. Please feel free to read more <Link to="/about">about me</Link>,
-        or you can check out my {' '}
-        <Link to="/resume">resume</Link>, {' '}
-        view <Link to="/stats">site statistics</Link>, {' '}
-        or <Link to="/contact">contact</Link> me.
-      </p>
-      <p> Source available <a href="https://github.com/Neelpatel1604/personal-site">here</a>.</p>
+      <div className="content">
+        <div className="logo-container" style={{ textAlign: 'center', marginBottom: '20px' }}>
+          <img
+            src="/images/cloudpractitionerlogo.png"
+            alt="Cloud Practitioner Certification"
+            style={{ width: '200px', height: 'auto' }}
+          />
+        </div>
+        <p>
+          Welcome to my website. Please feel free to read more <Link to="/about">about me</Link>,
+          or you can check out my {' '}
+          <Link to="/resume">resume</Link>, {' '}
+          view <Link to="/stats">site statistics</Link>, {' '}
+          or <Link to="/contact">contact</Link> me.
+        </p>
+        <p>
+          <a
+            href="https://www.credly.com/badges/a1a0c75d-b2bb-4f82-b97e-a67af59589b5/linked_in?t=shv1tc"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            View my Cloud Practitioner Certification
+          </a>
+        </p>
+        <p>Source available <a href="https://github.com/Neelpatel1604/personal-site">here</a>.</p>
+      </div>
     </article>
   </Main>
 );
