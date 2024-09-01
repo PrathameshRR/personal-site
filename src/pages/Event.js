c9b7804710458e11347a75676221188c09102a5f
Neelpatel1604 <neel_patel2004@outlook.com>
2024-08-31 23:12:01 -0400

    update event

--- src/pages/Event.js
+++ src/pages/Event.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import About from '../components/About';
 import EventLayout from '../components/Event/eventlayout';
-import events from '../data/event';
 
-const Event = () => (
-  <div className="event-page">
-    {events.map((event) => (
-      <EventLayout key={event.title} data={event} />
-    ))}
-  </div>
-);
+const EventPage = ({ events }) => {
+  return (
+    <div className="event-page-layout">
+      <div className="sidebar">
+        <About />
+      </div>
+      <div className="main-content">
+        <h1>Events</h1>
+        {events.map((event) => (
+          <EventLayout key={event.id} data={event} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default Event;
+export default EventPage;
