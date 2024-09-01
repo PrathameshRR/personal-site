ccb87b77115cee9192c0c239d1c816996b1415b5
Neelpatel1604 <neel_patel2004@outlook.com>
2024-08-31 23:18:35 -0400

    update event

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Suspense, lazy } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Main from './layouts/Main'; // fallback for lazy pages
 import './static/css/main.scss'; // All of our styles
 
@@ -13,23 +13,25 @@ const Contact = lazy(() => import('./pages/Contact'));
 const Index = lazy(() => import('./pages/Index'));
 const NotFound = lazy(() => import('./pages/NotFound'));
 const Projects = lazy(() => import('./pages/Projects'));
+const Event = lazy(() => import('./pages/Event'));
 const Resume = lazy(() => import('./pages/Resume'));
 const Stats = lazy(() => import('./pages/Stats'));
-const Event = lazy(() => import('./pages/Event'));
+const Hacker = lazy(() => import('./pages/Hacker'));
 
 const App = () => (
   <BrowserRouter basename={PUBLIC_URL}>
     <Suspense fallback={<Main />}>
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/stats" element={<Stats />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/resume" element={<Resume />} />
-        <Route path="/event" element={<Event />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Switch>
+        <Route exact path="/" component={Index} />
+        <Route path="/about" component={About} />
+        <Route path="/projects" component={Projects} />
+        <Route path="/stats" component={Stats} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/event" component={Event} />
+        <Route path="/resume" component={Resume} />
+        <Route path="/hacker" component={Hacker} />
+        <Route component={NotFound} status={404} />
+      </Switch>
     </Suspense>
   </BrowserRouter>
 );
--- src/pages/Event.js
+++ src/pages/Event.js
@@ -1,21 +1,13 @@
 import React from 'react';
-import About from '../components/About';
 import EventLayout from '../components/Event/eventlayout';
+import events from '../data/event';
 
-const EventPage = ({ events }) => {
-  return (
-    <div className="event-page-layout">
-      <div className="sidebar">
-        <About />
-      </div>
-      <div className="main-content">
-        <h1>Events</h1>
-        {events.map((event) => (
-          <EventLayout key={event.id} data={event} />
-        ))}
-      </div>
-    </div>
-  );
-};
+const Event = () => (
+  <div className="event-page">
+    {events.map((event) => (
+      <EventLayout key={event.title} data={event} />
+    ))}
+  </div>
+);
 
-export default EventPage;
+export default Event;
