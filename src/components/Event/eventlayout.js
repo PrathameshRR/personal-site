1dadf0ea43d5be20ebfff96d991c47a468b9cb5f
Neelpatel1604 <neel_patel2004@outlook.com>
2024-09-03 13:53:30 -0400

    update layout

--- src/components/Event/eventlayout.js
+++ src/components/Event/eventlayout.js
@@ -74,7 +74,7 @@ EventLayout.propTypes = {
           url: PropTypes.string.isRequired,
           link: PropTypes.string,
         }),
-      ])
+      ]),
     ).isRequired,
     date: PropTypes.string.isRequired,
   }).isRequired,
