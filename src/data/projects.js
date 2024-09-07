2113fc6233e10ddb83afb5bdeb97d6dc206a2eaa
Neelpatel1604 <neel_patel2004@outlook.com>
2024-09-06 22:51:37 -0400

    update series

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,7 +1,7 @@
 // TODO Add a couple lines about each project
 const data = [
   {
-    title: 'Deployment Of ReactJS',
+    title: 'Deployment Of ReactJS by Using Docker and EC2',
     subtitle: 'Deployement by using Docker and EC2',
     image: '/images/projects/Deploymentofreactjsapp.png',
     link: 'https://blog.neelbuilds.com/deployment-for-react-js',
@@ -12,6 +12,20 @@ const data = [
       + 'It covers preparation, hosting, configuration, and focuses  '
       + 'on performance optimization to '
       + 'ensure a seamless deployment process.',
+  },
+  {
+    title: 'AWS Cloud Paractitioner Series',
+    subtitle: 'AWS Cloud Practitioner Series with Neel Patel',
+    image: '/images/projects/Deploymentofreactjsapp.png',
+    link: 'https://blog.neelbuilds.com/series/awscloudpractitioner',
+    startDate: '2024-05-18',
+    endDate: '2024-06-11',
+    desc:
+      'This blog series covers essential topics for the'
+      + 'AWS Cloud Practitioner certification,'
+      +' including AWS services, cloud basics,'
+      + 'security, and pricing. It is ideal for beginners aiming'
+      + 'to understand AWS and pass the certification exam.',
   },
 ];
 
