63802d4f2b53315b3668cfdf3760ab580d62f217
Neelpatel1604 <neel_patel2004@outlook.com>
2024-12-17 14:02:55 -0500

    Services update

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const Contact = lazy(() => import('./pages/Contact'));
 const Index = lazy(() => import('./pages/Index'));
 const NotFound = lazy(() => import('./pages/NotFound'));
 const Projects = lazy(() => import('./pages/Projects'));
-const Services= lazy(() => import ('./pages/Services'))
+const Services = lazy(() => import('./pages/Services'));
 const Customers = lazy(() => import('./pages/Customers'));
 const Event = lazy(() => import('./pages/Event'));
 const Resume = lazy(() => import('./pages/Resume'));
--- src/data/services.js
+++ src/data/services.js
@@ -26,17 +26,17 @@ const data = [
     ],
   },
   {
-    'title': 'Bus Crowd Detection System',
-    'subtitle': 'Real-time crowd monitoring with person detection and email notifications',
-    'categories': ['Web-Development'],
-    'link': 'https://photosevent.s3.us-east-1.amazonaws.com/Crowd+Detection.mp4',
-    'additionalInfo': [
-        'Real-time person detection with YOLOv4-tiny.',
-        'Email alerts for overcrowding with cooldown settings.',
-        'Stores detection data in SQLite for analysis.',
-        'Secure email credentials with environment variables.',
-        'Easy deployment on edge devices for real-time use.'
-    ]
+    title: 'Bus Crowd Detection System',
+    subtitle: 'Real-time crowd monitoring with person detection and email notifications',
+    categories: ['Web-Development'],
+    link: 'https://photosevent.s3.us-east-1.amazonaws.com/Crowd+Detection.mp4',
+    additionalInfo: [
+      'Real-time person detection with YOLOv4-tiny.',
+      'Email alerts for overcrowding with cooldown settings.',
+      'Stores detection data in SQLite for analysis.',
+      'Secure email credentials with environment variables.',
+      'Easy deployment on edge devices for real-time use.',
+    ],
   },
   {
     title: 'Math Solver',
@@ -50,60 +50,59 @@ const data = [
       'Developed React frontend, hosted on Netlify.',
       'Hosted backend on Render with GitHub integration.',
     ],
-  },  
-  {
-      title: 'Urban Global: Solution for Sustainable Brampton Transit',
-      subtitle: 'Empowering Sustainable Commuting in Brampton Through Multi-Modal Transit Options',
-      categories: ['Web-Developement'],
-      link: 'https://photosevent.s3.us-east-1.amazonaws.com/UrbanGlobalVideo.mp4',
-      additionalInfo: [
-        'Built with React for a responsive interface.',
-        'Promotes bus, carpool, and cycling options.',
-        'Simulates real-time data for transit planning.',
-        'Supports sustainability and eco-friendly commuting.',
-        'Future plans for backend integration and rewards.',
-      ],
-    },
-    {
-      title: 'Neel Builds',
-      subtitle: 'Enhancing Web Performance with ReactJS, CI/CD, and Cloud Hosting',
-      categories: ['Web-Developement'],
-      link: 'https://blog.neelbuilds.com/personal-website',
-      additionalInfo: [
-        'Forked code and enhanced skills in ReactJS.',
-        'Deployed on Netlify with CI/CD and custom domains.',
-        'Managed domain using Cloudflare.',
-        'Stored photos on AWS S3 with CloudFront for fast delivery.',
-        'Learned streamlined workflows and continuous deployment.',
-      ],
-    },
-    {
-      title: 'Deploy React App with Docker & EC2',
-      subtitle: 'Reducing down time by 99%',
-      categories: ['Cloud & DevOps'],
-      link: 'https://blog.neelbuilds.com/deployment-for-react-js',
-      additionalInfo: [
-        'Used Docker for consistent deployment across environments.',
-        'Pushed the Docker image to Docker Hub for easy access.',
-        'Deployed the app on an AWS EC2 instance for hosting.',
-        'Configured security settings to allow inbound traffic on port 80.',
-        'Optimized the app with multi-stage builds and caching for better performance.',
-      ],
-    },
-    {
-      'title': 'AWS Cloud Practitioner Blog Series',
-      'subtitle': 'Comprehensive guide to mastering AWS Cloud Fundamentals',
-      'categories': ['Technical Writing', 'Cloud & DevOps'],
-      'link': 'https://blog.neelbuilds.com/series/awscloudpractitioner',
-      'additionalInfo': [
-          'Step-by-step tutorials on core AWS services and concepts.',
-          'Covers essential topics like EC2, S3, VPC, IAM, and more.',
-          'Helps beginners prepare for the AWS Certified Cloud Practitioner exam.',
-          'Provides practical examples and real-world use cases.',
-          'Regularly updated with new AWS features and best practices.'
-      ]
   },
-
+  {
+    title: 'Urban Global: Solution for Sustainable Brampton Transit',
+    subtitle: 'Empowering Sustainable Commuting in Brampton Through Multi-Modal Transit Options',
+    categories: ['Web-Developement'],
+    link: 'https://photosevent.s3.us-east-1.amazonaws.com/UrbanGlobalVideo.mp4',
+    additionalInfo: [
+      'Built with React for a responsive interface.',
+      'Promotes bus, carpool, and cycling options.',
+      'Simulates real-time data for transit planning.',
+      'Supports sustainability and eco-friendly commuting.',
+      'Future plans for backend integration and rewards.',
+    ],
+  },
+  {
+    title: 'Neel Builds',
+    subtitle: 'Enhancing Web Performance with ReactJS, CI/CD, and Cloud Hosting',
+    categories: ['Web-Developement'],
+    link: 'https://blog.neelbuilds.com/personal-website',
+    additionalInfo: [
+      'Forked code and enhanced skills in ReactJS.',
+      'Deployed on Netlify with CI/CD and custom domains.',
+      'Managed domain using Cloudflare.',
+      'Stored photos on AWS S3 with CloudFront for fast delivery.',
+      'Learned streamlined workflows and continuous deployment.',
+    ],
+  },
+  {
+    title: 'Deploy React App with Docker & EC2',
+    subtitle: 'Reducing down time by 99%',
+    categories: ['Cloud & DevOps'],
+    link: 'https://blog.neelbuilds.com/deployment-for-react-js',
+    additionalInfo: [
+      'Used Docker for consistent deployment across environments.',
+      'Pushed the Docker image to Docker Hub for easy access.',
+      'Deployed the app on an AWS EC2 instance for hosting.',
+      'Configured security settings to allow inbound traffic on port 80.',
+      'Optimized the app with multi-stage builds and caching for better performance.',
+    ],
+  },
+  {
+    title: 'AWS Cloud Practitioner Blog Series',
+    subtitle: 'Comprehensive guide to mastering AWS Cloud Fundamentals',
+    categories: ['Technical Writing', 'Cloud & DevOps'],
+    link: 'https://blog.neelbuilds.com/series/awscloudpractitioner',
+    additionalInfo: [
+      'Step-by-step tutorials on core AWS services and concepts.',
+      'Covers essential topics like EC2, S3, VPC, IAM, and more.',
+      'Helps beginners prepare for the AWS Certified Cloud Practitioner exam.',
+      'Provides practical examples and real-world use cases.',
+      'Regularly updated with new AWS features and best practices.',
+    ],
+  },
 ];
 
 export default data;
